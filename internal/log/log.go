724a68e54a12bd93a37021e0a37f073ac04f6aa6
Alex Goodman <alex.goodman@anchore.com>
2021-04-03 08:59:18 -0400

    add field logger
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/log/log.go
+++ internal/log/log.go
@@ -4,22 +4,42 @@ import "github.com/anchore/stereoscope/pkg/logger"
 
 var Log logger.Logger = &nopLogger{}
 
+// Errorf takes a formatted template string and template arguments for the error logging level.
 func Errorf(format string, args ...interface{}) {
 	Log.Errorf(format, args...)
 }
 
+// Error logs the given arguments at the error logging level.
+func Error(args ...interface{}) {
+	Log.Error(args...)
+}
+
+// Warnf takes a formatted template string and template arguments for the warning logging level.
+func Warnf(format string, args ...interface{}) {
+	Log.Warnf(format, args...)
+}
+
+// Warn logs the given arguments at the warning logging level.
+func Warn(args ...interface{}) {
+	Log.Warn(args...)
+}
+
+// Infof takes a formatted template string and template arguments for the info logging level.
 func Infof(format string, args ...interface{}) {
 	Log.Infof(format, args...)
 }
 
+// Info logs the given arguments at the info logging level.
 func Info(args ...interface{}) {
 	Log.Info(args...)
 }
 
+// Debugf takes a formatted template string and template arguments for the debug logging level.
 func Debugf(format string, args ...interface{}) {
 	Log.Debugf(format, args...)
 }
 
-func Debug(args ...interface{}) {
-	Log.Debug(args...)
+// WithFields returns a message logger with multiple key-value fields.
+func WithFields(fields ...interface{}) logger.MessageLogger {
+	return Log.WithFields(fields...)
 }
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,7 +1,19 @@
 package logger
 
 type Logger interface {
+	MessageLogger
+	FieldLogger
+}
+
+type FieldLogger interface {
+	WithFields(fields ...interface{}) MessageLogger
+}
+
+type MessageLogger interface {
 	Errorf(format string, args ...interface{})
+	Error(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Warn(args ...interface{})
 	Infof(format string, args ...interface{})
 	Info(args ...interface{})
 	Debugf(format string, args ...interface{})
