3b7662f3a66f75d4964b2c6577abeb93b0871d9e
Dan Luhring <luhring@users.noreply.github.com>
2021-05-24 13:52:38 -0400

    Allow for empty authority value and refactor (#69)
    * Allow for empty authority value and refactor
    
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>
    
    * Add t.Helper calls in credentials test helpers
    
    Signed-off-by: Dan Luhring <dan.luhring@anchore.com>

--- pkg/image/registry_options.go
+++ pkg/image/registry_options.go
@@ -11,34 +11,21 @@ type RegistryOptions struct {
 	Credentials           []RegistryCredentials
 }
 
-// RegistryCredentials contains any information necessary to authenticate against an OCI-distribution-compliant
-// registry (either with basic auth or bearer token). Note: only valid for the OCI registry provider.
-type RegistryCredentials struct {
-	Authority string
-	Username  string
-	Password  string
-	Token     string
-}
-
 // Authenticator returns an object capable of authenticating against the given registry. If no credentials match the
 // given registry, or there is partial information configured, then nil is returned.
-func (r *RegistryOptions) Authenticator(registry string) authn.Authenticator {
-	for idx, auth := range r.Credentials {
-		if auth.Authority != registry {
+func (r RegistryOptions) Authenticator(registry string) authn.Authenticator {
+	for idx, credentials := range r.Credentials {
+		if !credentials.canBeUsedWithRegistry(registry) {
 			continue
 		}
-		if auth.Username != "" && auth.Password != "" {
-			log.Debugf("using registry credentials for %q (config idx=%d)", auth.Authority, idx)
-			return &authn.Basic{
-				Username: auth.Username,
-				Password: auth.Password,
-			}
-		} else if auth.Token != "" {
-			log.Debugf("using registry token for %q (config idx=%d)", auth.Authority, idx)
-			return &authn.Bearer{
-				Token: auth.Token,
-			}
+
+		authenticator := credentials.authenticator()
+		if authenticator == nil {
+			continue
 		}
+
+		log.Debugf("using registry credentials from config index %d", idx)
+		return authenticator
 	}
 
 	return nil
--- pkg/image/registry_options_test.go
+++ pkg/image/registry_options_test.go
@@ -1,22 +1,22 @@
 package image
 
 import (
-	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/google/go-containerregistry/pkg/authn"
 )
 
-func TestRegistryOptions_Authenticator_Basic(t *testing.T) {
+func TestRegistryOptions_Authenticator(t *testing.T) {
 	tests := []struct {
-		name     string
-		registry string
-		input    *RegistryOptions
-		expected *authn.Basic
+		name                   string
+		registry               string
+		input                  RegistryOptions
+		authenticatorAssertion func(t *testing.T, actual authn.Authenticator)
 	}{
 		{
-			name:     "require user pass success",
+			name:     "basicAuth credentials match registry",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost:5000",
@@ -25,15 +25,15 @@ func TestRegistryOptions_Authenticator_Basic(t *testing.T) {
 					},
 				},
 			},
-			expected: &authn.Basic{
+			authenticatorAssertion: basicAuth(authn.Basic{
 				Username: "username",
 				Password: "tOpsYKrets",
-			},
+			}),
 		},
 		{
-			name:     "mismatched registry",
+			name:     "basicAuth credentials don't match registry",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost",
@@ -42,60 +42,42 @@ func TestRegistryOptions_Authenticator_Basic(t *testing.T) {
 					},
 				},
 			},
-			expected: nil,
+			authenticatorAssertion: nilAuthenticator(),
 		},
 		{
-			name:     "no password",
+			name:     "authority with missing credentials",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
-				Credentials: []RegistryCredentials{
-					{
-						Authority: "localhost:5000",
-						Username:  "username",
-					},
-				},
-			},
-			expected: nil,
-		},
-		{
-			name:     "no username",
-			registry: "localhost:5000",
-			input: &RegistryOptions{
-				Credentials: []RegistryCredentials{
-					{
-						Authority: "localhost:5000",
-						Password:  "awesome-sauce",
-					},
-				},
-			},
-			expected: nil,
-		},
-		{
-			name:     "no username or password",
-			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost:5000",
 					},
 				},
 			},
-			expected: nil,
+			authenticatorAssertion: nilAuthenticator(),
 		},
 		{
 			name:     "empty struct",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{},
 				},
 			},
-			expected: nil,
+			authenticatorAssertion: nilAuthenticator(),
 		},
 		{
-			name:     "multiple matches",
+			name:     "empty credentials slice",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
+				Credentials: []RegistryCredentials{},
+			},
+			authenticatorAssertion: nilAuthenticator(),
+		},
+		{
+			name:     "given multiple matches, select first match",
+			registry: "localhost:5000",
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost:5000",
@@ -109,61 +91,31 @@ func TestRegistryOptions_Authenticator_Basic(t *testing.T) {
 					},
 				},
 			},
-			expected: &authn.Basic{
+			authenticatorAssertion: basicAuth(authn.Basic{
 				Username: "username",
 				Password: "tOpsYKrets",
-			},
+			}),
 		},
 		{
-			name:     "basic auth over bearer",
+			name:     "basic auth without authority",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
-						Authority: "localhost:5000",
-						Username:  "username",
-						Password:  "tOpsYKrets",
-					},
-					{
-						Authority: "localhost:5000",
-						Token:     "BLERG",
+						Username: "username",
+						Password: "tOpsYKrets",
 					},
 				},
 			},
-			expected: &authn.Basic{
+			authenticatorAssertion: basicAuth(authn.Basic{
 				Username: "username",
 				Password: "tOpsYKrets",
-			},
+			}),
 		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			actualAuth := test.input.Authenticator(test.registry)
-			if test.expected == nil && actualAuth == nil {
-				// pass
-				return
-			}
-			actual, ok := actualAuth.(*authn.Basic)
-			if !ok {
-				t.Fatalf("unable to get basic auth obj: %+v", actualAuth)
-			}
-			assert.Equal(t, test.expected, actual)
-		})
-	}
-}
-
-func TestRegistryOptions_Authenticator_Bearer(t *testing.T) {
-	tests := []struct {
-		name     string
-		registry string
-		input    *RegistryOptions
-		expected *authn.Bearer
-	}{
 		{
-			name:     "require bearer token success",
+			name:     "bearer token credentials match registry",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost:5000",
@@ -171,26 +123,14 @@ func TestRegistryOptions_Authenticator_Bearer(t *testing.T) {
 					},
 				},
 			},
-			expected: &authn.Bearer{
+			authenticatorAssertion: bearerToken(authn.Bearer{
 				Token: "JRR",
-			},
+			}),
 		},
 		{
-			name:     "missing bearer token",
+			name:     "bearer token credentials don't match registry",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
-				Credentials: []RegistryCredentials{
-					{
-						Authority: "localhost:5000",
-					},
-				},
-			},
-			expected: nil,
-		},
-		{
-			name:     "mismatched registry",
-			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Authority: "localhost",
@@ -198,34 +138,28 @@ func TestRegistryOptions_Authenticator_Bearer(t *testing.T) {
 					},
 				},
 			},
-			expected: nil,
+			authenticatorAssertion: nilAuthenticator(),
 		},
 		{
-			name:     "missing registry",
+			name:     "bearer token without authority",
 			registry: "localhost:5000",
-			input: &RegistryOptions{
+			input: RegistryOptions{
 				Credentials: []RegistryCredentials{
 					{
 						Token: "JRR",
 					},
 				},
 			},
-			expected: nil,
+			authenticatorAssertion: bearerToken(authn.Bearer{
+				Token: "JRR",
+			}),
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			actualAuth := test.input.Authenticator(test.registry)
-			if test.expected == nil && actualAuth == nil {
-				// pass
-				return
-			}
-			actual, ok := actualAuth.(*authn.Bearer)
-			if !ok {
-				t.Fatalf("unable to get bearer obj: %+v", actualAuth)
-			}
-			assert.Equal(t, test.expected, actual)
+			test.authenticatorAssertion(t, actualAuth)
 		})
 	}
 }
