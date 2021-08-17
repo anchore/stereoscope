0f4abc2a5a5ac9f57869b272402e58f613565d29
Keith Zantow <kzantow@gmail.com>
2021-08-17 12:05:04 -0400

    Refactor prepareReferenceOptions and tests (#76)
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

---
+++ pkg/image/oci/registry_provider_test.go
@@ -0,0 +1,46 @@
+package oci
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/anchore/stereoscope/pkg/image"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_prepareReferenceOptions(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    image.RegistryOptions
+		expected []name.Option
+	}{
+		{
+			name:     "not InsecureUseHTTP",
+			input:    image.RegistryOptions{},
+			expected: nil,
+		},
+		{
+			name: "use InsecureUseHTTP",
+			input: image.RegistryOptions{
+				InsecureUseHTTP: true,
+			},
+			expected: []name.Option{name.Insecure},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			out := prepareReferenceOptions(&test.input)
+			assert.Equal(t, len(test.expected), len(out))
+			if test.expected == nil {
+				assert.Equal(t, test.expected, out)
+			} else {
+				// cannot compare functions directly
+				e1 := reflect.ValueOf(test.expected[0])
+				e2 := reflect.ValueOf(out[0])
+				assert.Equal(t, e1, e2)
+			}
+		})
+	}
+}
