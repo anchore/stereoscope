72a9cd495d78d46b189f1cb8e6ce48fe0a387df3
Jonas Galvão Xavier <jonas.agx@gmail.com>
2022-01-20 15:50:50 -0800

    podman - inte test without QEMU
    tries to ssh into localhost to validate ssh client
    
    Signed-off-by: Jonas Galvão Xavier <jonas.agx@gmail.com>

--- test/integration/podman_test.go
+++ test/integration/podman_test.go
@@ -2,12 +2,18 @@ package integration
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/anchore/stereoscope/internal/podman"
 	"github.com/docker/docker/client"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // test conditions:
@@ -28,30 +34,63 @@ func TestPodmanConnections(t *testing.T) {
 	tests := []struct {
 		name        string
 		constructor func() (*client.Client, error)
+		setup       func(*testing.T)
 	}{
 		{
 			name:        "ssh connection",
 			constructor: podman.ClientOverSSH,
+			setup:       setupSSHKeys,
 		},
 		{
 			name:        "unix socket connection",
 			constructor: podman.ClientOverUnixSocket,
+			setup:       func(t *testing.T) {},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client, err := tt.constructor()
+			tt.setup(t)
+			c, err := tt.constructor()
 			assert.NoError(t, err)
-			assert.NotEmpty(t, client.ClientVersion())
+			assert.NotEmpty(t, c.ClientVersion())
 
-			p, err := client.Ping(context.Background())
+			p, err := c.Ping(context.Background())
 			assert.NoError(t, err)
 			assert.NotNil(t, p)
 
-			version, err := client.ServerVersion(context.Background())
+			version, err := c.ServerVersion(context.Background())
 			assert.NoError(t, err)
 			assert.NotEmpty(t, version)
 		})
 	}
+}
+
+func setupSSHKeys(t *testing.T) {
+	require.Equalf(t, "linux", runtime.GOOS, "setup meant for CI -- it can modify your ssh authorized keys")
+
+	addr := fmt.Sprintf("ssh://localhost/run/user/%d/podman/podman.sock", os.Getuid())
+	err := os.Setenv("CONTAINER_HOST", addr)
+	assert.NoError(t, err)
+	// ssh-keygen -t rsa -f test-rsa -N "passphrase"
+	keyFile := filepath.Join(os.TempDir(), "integration-test-key")
+
+	err = os.Setenv("CONTAINER_SSHKEY", keyFile)
+	assert.NoError(t, err)
+
+	// 0: making key
+	out, err := exec.Command("ssh-keygen", "-t", "rsa", "-f", keyFile, "-N", "").Output()
+	require.NoError(t, err)
+	t.Logf("output: %s", out)
+
+	// 1: append to authorized_keys
+	b, err := ioutil.ReadFile(keyFile + ".pub")
+	require.NoError(t, err)
+
+	// If the file doesn't exist, create it, or append to the file
+	f, err := os.OpenFile("~/.ssh/authorized_keys", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	require.NoError(t, err)
+	size, err := f.Write(b)
+	require.NoError(t, err)
+	assert.NotZero(t, size)
 }
