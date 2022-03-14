62daa1e1d05e67f0ee88af483a42cf6fbea04805
Jonas Galvão Xavier <jonas.agx@gmail.com>
2022-03-14 11:05:08 -0700

    better testing
    Signed-off-by: Jonas Galvão Xavier <jonas.agx@gmail.com>

--- pkg/file/lazy_bounded_read_closer_test.go
+++ pkg/file/lazy_bounded_read_closer_test.go
@@ -1,114 +1,48 @@
 package file
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func getFixture(t *testing.T, filepath string) []byte {
+	p := "test-fixtures/a-file.txt"
+	fh, err := os.Open(p)
+	require.NoError(t, err)
+	expectedContents, err := ioutil.ReadAll(fh)
+	require.NoError(t, err)
+
+	return expectedContents
+}
 
 func TestDeferredPartialReadCloser(t *testing.T) {
 	p := "test-fixtures/a-file.txt"
-	fh, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedContents, err := ioutil.ReadAll(fh)
-	if err != nil {
-		t.Fatal(err)
-	}
+	contents := getFixture(t, p)
 
-	dReader := newLazyBoundedReadCloser(p, 0, int64(len(expectedContents)))
-
-	if dReader.file != nil {
-		t.Fatalf("should not have a file, but we do somehow")
-	}
+	dReader := newLazyBoundedReadCloser(p, 0, int64(len(contents)))
+	require.Nil(t, dReader.file)
 
 	actualContents, err := ioutil.ReadAll(dReader)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if !bytes.Equal(expectedContents, actualContents) {
-		t.Fatalf("unexpected contents: %s", string(actualContents))
-	}
+	require.Equal(t, contents, actualContents)
+	require.NotNil(t, dReader.file)
 
-	if dReader.file == nil {
-		t.Fatalf("should have a file, but we do not somehow")
-	}
-
-	if err := dReader.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	if dReader.file != nil {
-		t.Fatalf("should not have a file, but we do somehow")
-	}
+	require.NoError(t, dReader.Close())
+	require.Nil(t, dReader.file, "should not have a file, but we do somehow")
 }
 
 func TestDeferredPartialReadCloser_PartialRead(t *testing.T) {
 	p := "test-fixtures/a-file.txt"
-	fh, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	contents, err := ioutil.ReadAll(fh)
-	if err != nil {
-		t.Fatal(err)
-	}
+	contents := getFixture(t, p)
 
-	var start, size = 10, 7
-	dReader := newLazyBoundedReadCloser(p, int64(start), int64(size))
+	var start, size int64 = 10, 7
+	dReader := newLazyBoundedReadCloser(p, start, size)
 
 	actualContents, err := ioutil.ReadAll(dReader)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !bytes.Equal(contents[start:start+size], actualContents) {
-		t.Fatalf("unexpected contents: %s", string(actualContents))
-	}
-
-}
-
-func Test_lazyBoundedReadCloser_Read(t *testing.T) {
-	type fields struct {
-		path   string
-		file   *os.File
-		reader *io.SectionReader
-		start  int64
-		size   int64
-	}
-	type args struct {
-		b []byte
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    int
-		wantErr assert.ErrorAssertionFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &lazyBoundedReadCloser{
-				path:   tt.fields.path,
-				file:   tt.fields.file,
-				reader: tt.fields.reader,
-				start:  tt.fields.start,
-				size:   tt.fields.size,
-			}
-			got, err := d.Read(tt.args.b)
-			if !tt.wantErr(t, err, fmt.Sprintf("Read(%v)", tt.args.b)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "Read(%v)", tt.args.b)
-		})
-	}
+	require.NoError(t, err)
+	require.Equal(t, contents[start:start+size], actualContents)
 }
--- pkg/file/lazy_read_closer_test.go
+++ pkg/file/lazy_read_closer_test.go
@@ -1,47 +1,44 @@
 package file
 
 import (
-	"bytes"
 	"io/ioutil"
-	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestDeferredReadCloser(t *testing.T) {
-	p := "test-fixtures/a-file.txt"
-	fh, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expectedContents, err := ioutil.ReadAll(fh)
-	if err != nil {
-		t.Fatal(err)
-	}
+	filepath := "test-fixtures/a-file.txt"
+	allContent := getFixture(t, filepath)
 
-	dReader := NewLazyReadCloser(p)
-
-	if dReader.file != nil {
-		t.Fatalf("should not have a file, but we do somehow")
-	}
+	dReader := NewLazyReadCloser(filepath)
+	require.Nil(t, dReader.file, "should not have a file, but we do somehow")
 
 	actualContents, err := ioutil.ReadAll(dReader)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NotNil(t, dReader.file, "should have a file, but we do not somehow")
+	require.NoError(t, err)
+	require.Equal(t, allContent, actualContents)
 
-	if !bytes.Equal(expectedContents, actualContents) {
-		t.Fatalf("unexpected contents: %s", string(actualContents))
-	}
+	require.NoError(t, dReader.Close())
+	require.Nil(t, dReader.file, "should not have a file, but we do somehow")
+}
 
-	if dReader.file == nil {
-		t.Fatalf("should have a file, but we do not somehow")
-	}
+func TestLazyReader_ReatAt(t *testing.T) {
+	filepath := "test-fixtures/a-file.txt"
+	allContent := getFixture(t, filepath)
 
-	if err := dReader.Close(); err != nil {
-		t.Fatal(err)
-	}
+	dReader := NewLazyReadCloser(filepath)
+	require.Nil(t, dReader.file, "should not have a file, but we do somehow")
 
-	if dReader.file != nil {
-		t.Fatalf("should not have a file, but we do somehow")
-	}
+	off := 5
+	left := len(allContent) - off
+	s := make([]byte, left)
+	n, err := dReader.ReadAt(s, int64(off))
+	require.NoError(t, err)
+	require.Equal(t, left, n)
+	require.Equal(t, allContent[off:], s)
+
+	require.NoError(t, dReader.Close())
+	require.Nil(t, dReader.file, "should not have a file, but we do somehow")
+
 }
