aa4918fe9c1975560f345e7ffac2acc02841e3d0
Alex Goodman <alex.goodman@anchore.com>
2022-03-29 12:13:42 -0400

    [wip] partial PoC for multi manifest support in tarball provider
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- pkg/image/docker/tarball_provider.go
+++ pkg/image/docker/tarball_provider.go
@@ -1,9 +1,15 @@
 package docker
 
 import (
+	"archive/tar"
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/google/go-containerregistry/pkg/name"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
 
 	"github.com/anchore/stereoscope/internal/log"
 	"github.com/anchore/stereoscope/pkg/file"
@@ -34,7 +40,15 @@ func (p *TarballImageProvider) Provide(_ context.Context, userMetadata ...image.
 	if err != nil {
 		// raise a more controlled error for when there are multiple images within the given tar (from https://github.com/anchore/grype/issues/215)
 		if err.Error() == "tarball must contain only a single image to be used with tarball.Image" {
-			return nil, ErrMultipleManifests
+			images, err := multiImage(pathOpener(p.path))
+			if err != nil {
+				// TODO: nope
+				panic(err)
+			}
+
+			/// TODO: now what? we can't reconcile a single image out
+
+			//return nil, ErrMultipleManifests
 		}
 		return nil, fmt.Errorf("unable to provide image from tarball: %w", err)
 	}
@@ -83,4 +97,89 @@ func (p *TarballImageProvider) Provide(_ context.Context, userMetadata ...image.
 	}
 
 	return image.NewImage(img, contentTempDir, metadata...), nil
+}
+
+// code extracted from open PR: https://github.com/google/go-containerregistry/pull/1110
+
+func multiImage(opener tarball.Opener, opt ...name.Option) (map[name.Reference]v1.Image, error) {
+	m, err := extractFileFromTar(opener, "manifest.json")
+	if err != nil {
+		return nil, err
+	}
+	var iManifest = make(tarball.Manifest, 0)
+	defer m.Close()
+	if err = json.NewDecoder(m).Decode(&iManifest); err != nil {
+		return nil, err
+	}
+	var out = make(map[name.Reference]v1.Image, len(iManifest))
+	for i := range iManifest {
+		manifest := iManifest[i]
+		if len(manifest.RepoTags) == 0 {
+			return nil, fmt.Errorf("repo tags missing from manifest config %s", manifest.Config)
+		}
+		// TODO: does the first entry make sense?
+		ref, err := name.ParseReference(manifest.RepoTags[0], opt...)
+		if err != nil {
+			return nil, err
+		}
+		t, _ := name.NewTag(ref.String())
+		if err != nil {
+			return nil, err
+		}
+		v1Image, err := tarball.Image(opener, &t)
+
+		if err != nil {
+			return nil, err
+		}
+		out[ref] = v1Image
+	}
+	return out, nil
+}
+
+func pathOpener(path string) tarball.Opener {
+	return func() (io.ReadCloser, error) {
+		return os.Open(path)
+	}
+}
+
+func extractFileFromTar(opener tarball.Opener, filePath string) (io.ReadCloser, error) {
+	f, err := opener()
+	if err != nil {
+		return nil, err
+	}
+	close := true
+	defer func() {
+		if close {
+			f.Close()
+		}
+	}()
+
+	tf := tar.NewReader(f)
+	for {
+		hdr, err := tf.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		if hdr.Name == filePath {
+			if hdr.Typeflag == tar.TypeSymlink || hdr.Typeflag == tar.TypeLink {
+				currentDir := filepath.Dir(filePath)
+				return extractFileFromTar(opener, path.Join(currentDir, hdr.Linkname))
+			}
+			close = false
+			return tarFile{
+				Reader: tf,
+				Closer: f,
+			}, nil
+		}
+	}
+	return nil, fmt.Errorf("file %s not found in tar", filePath)
+}
+
+// tarFile represents a single file inside a tar. Closing it closes the tar itself.
+type tarFile struct {
+	io.Reader
+	io.Closer
 }
