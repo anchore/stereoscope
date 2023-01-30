58b595c47de1aab9ea5c8190d794503ac80e4b64
Alex Goodman <alex.goodman@anchore.com>
2023-01-30 15:17:26 -0500

    add tests for new search context object
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

---
+++ pkg/filetree/search_test.go
@@ -0,0 +1,253 @@
+package filetree
+
+import (
+	"fmt"
+	"github.com/anchore/stereoscope/pkg/file"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_searchContext_SearchByPath(t *testing.T) {
+	type fields struct {
+		tree  *FileTree
+		index Index
+	}
+	type args struct {
+		path    string
+		options []LinkResolutionOption
+	}
+
+	tree := NewFileTree()
+	ref, err := tree.AddFile("/path/to/file.txt")
+	require.NoError(t, err)
+	require.NotNil(t, ref)
+
+	idx := NewIndex()
+	idx.Add(*ref, file.Metadata{MIMEType: "plain/text"})
+
+	defaultFields := fields{
+		tree:  tree,
+		index: idx,
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *file.ReferenceAccessVia
+		wantErr require.ErrorAssertionFunc
+	}{
+		{
+			name:   "path exists",
+			fields: defaultFields,
+			args: args{
+				path: "/path/to/file.txt",
+			},
+			want: &file.ReferenceAccessVia{
+				ReferenceAccess: file.ReferenceAccess{
+					RequestPath: "/path/to/file.txt",
+					Reference: &file.Reference{
+						RealPath: "/path/to/file.txt",
+					},
+				},
+			},
+		},
+		{
+			name:   "path does not exists",
+			fields: defaultFields,
+			args: args{
+				path: "/NOT/path/to/file.txt",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr == nil {
+				tt.wantErr = require.NoError
+			}
+			i := searchContext{
+				tree:  tt.fields.tree,
+				index: tt.fields.index,
+			}
+			got, err := i.SearchByPath(tt.args.path, tt.args.options...)
+			tt.wantErr(t, err, fmt.Sprintf("SearchByPath(%v, %v)", tt.args.path, tt.args.options))
+			if err != nil {
+				return
+			}
+
+			opts := []cmp.Option{
+				cmpopts.IgnoreFields(file.Reference{}, "id"),
+			}
+
+			if d := cmp.Diff(tt.want, got, opts...); d != "" {
+				t.Errorf("SearchByPath() mismatch (-want +got):\n%s", d)
+			}
+		})
+	}
+}
+
+func Test_searchContext_SearchByGlob(t *testing.T) {
+	type fields struct {
+		tree  *FileTree
+		index Index
+	}
+	type args struct {
+		glob    string
+		options []LinkResolutionOption
+	}
+
+	tree := NewFileTree()
+	ref, err := tree.AddFile("/path/to/file.txt")
+	require.NoError(t, err)
+	require.NotNil(t, ref)
+
+	idx := NewIndex()
+	idx.Add(*ref, file.Metadata{MIMEType: "plain/text"})
+
+	defaultFields := fields{
+		tree:  tree,
+		index: idx,
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []file.ReferenceAccessVia
+		wantErr require.ErrorAssertionFunc
+	}{
+		{
+			name:   "path exists",
+			fields: defaultFields,
+			args: args{
+				glob: "/**/t?/fil?.txt",
+			},
+			want: []file.ReferenceAccessVia{
+				{
+					ReferenceAccess: file.ReferenceAccess{
+						RequestPath: "/path/to/file.txt",
+						Reference: &file.Reference{
+							RealPath: "/path/to/file.txt",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "path does not exists",
+			fields: defaultFields,
+			args: args{
+				glob: "/NOT/**/file",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr == nil {
+				tt.wantErr = require.NoError
+			}
+			i := searchContext{
+				tree:  tt.fields.tree,
+				index: tt.fields.index,
+			}
+			got, err := i.SearchByGlob(tt.args.glob, tt.args.options...)
+			tt.wantErr(t, err, fmt.Sprintf("SearchByGlob(%v, %v)", tt.args.glob, tt.args.options))
+			if err != nil {
+				return
+			}
+
+			opts := []cmp.Option{
+				cmpopts.IgnoreFields(file.Reference{}, "id"),
+			}
+
+			if d := cmp.Diff(tt.want, got, opts...); d != "" {
+				t.Errorf("SearchByGlob() mismatch (-want +got):\n%s", d)
+			}
+		})
+	}
+}
+
+func Test_searchContext_SearchByMIMEType(t *testing.T) {
+	type fields struct {
+		tree  *FileTree
+		index Index
+	}
+	type args struct {
+		mimeTypes string
+	}
+
+	tree := NewFileTree()
+	ref, err := tree.AddFile("/path/to/file.txt")
+	require.NoError(t, err)
+	require.NotNil(t, ref)
+
+	idx := NewIndex()
+	idx.Add(*ref, file.Metadata{MIMEType: "plain/text"})
+
+	defaultFields := fields{
+		tree:  tree,
+		index: idx,
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []file.ReferenceAccessVia
+		wantErr require.ErrorAssertionFunc
+	}{
+		{
+			name:   "types exists",
+			fields: defaultFields,
+			args: args{
+				mimeTypes: "plain/text",
+			},
+			want: []file.ReferenceAccessVia{
+				{
+					ReferenceAccess: file.ReferenceAccess{
+						RequestPath: "/path/to/file.txt",
+						Reference: &file.Reference{
+							RealPath: "/path/to/file.txt",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:   "types do not exists",
+			fields: defaultFields,
+			args: args{
+				mimeTypes: "octetstream",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr == nil {
+				tt.wantErr = require.NoError
+			}
+			i := searchContext{
+				tree:  tt.fields.tree,
+				index: tt.fields.index,
+			}
+			got, err := i.SearchByMIMEType(tt.args.mimeTypes)
+			tt.wantErr(t, err, fmt.Sprintf("SearchByMIMEType(%v)", tt.args.mimeTypes))
+			if err != nil {
+				return
+			}
+
+			opts := []cmp.Option{
+				cmpopts.IgnoreFields(file.Reference{}, "id"),
+			}
+
+			if d := cmp.Diff(tt.want, got, opts...); d != "" {
+				t.Errorf("SearchByMIMEType() mismatch (-want +got):\n%s", d)
+			}
+		})
+	}
+}
