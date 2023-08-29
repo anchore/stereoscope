334c2222e137eeee780bd3779707d8a8cf879221
Alex Goodman <wagoodman@users.noreply.github.com>
2023-08-29 10:26:08 -0400

    Move TLS authentication material to registry credentials object (#195)
    * move tls material to registry credentials object
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * allow for CA file or directory
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * allow for using tls and basic auth credentials from different entries
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    * allow for overriding different levels of credentials from more specific sources
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- test/integration/test-fixtures/registry/test.go
+++ test/integration/test-fixtures/registry/test.go
@@ -17,16 +17,16 @@ func main() {
 		stereoscope.WithRegistryOptions(image.RegistryOptions{
 			InsecureSkipTLSVerify: false,
 			InsecureUseHTTP:       false,
+			CAFileOrDir:           "/certs/server.crt",
 			Credentials: []image.RegistryCredentials{
 				{
-					Authority: "registry.null:5000",
-					Username:  "testuser",
-					Password:  "testpass",
+					Authority:  "registry.null:5000",
+					Username:   "testuser42",
+					Password:   "testpass42",
+					ClientCert: "/certs/client.crt",
+					ClientKey:  "/certs/client.key",
 				},
 			},
-			CAFile:     "/certs/server.crt",
-			ClientCert: "/certs/client.crt",
-			ClientKey:  "/certs/client.key",
 		}),
 	)
 	if err != nil {
