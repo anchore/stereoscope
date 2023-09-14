061d521bcc916147d88684d959e1ad6357dd7546
Alex Goodman <wagoodman@users.noreply.github.com>
2023-09-14 13:14:23 -0400

    refactor containerd address resolution
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- internal/containerd/client.go
+++ internal/containerd/client.go
@@ -8,7 +8,12 @@ import (
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/defaults"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/spf13/afero"
+
+	"github.com/anchore/stereoscope/internal/log"
 )
+
+var ErrNoSocketAddress = fmt.Errorf("no socket address")
 
 func GetClient() (*containerd.Client, error) {
 	client, err := containerd.New(Address())
@@ -19,33 +24,11 @@ func GetClient() (*containerd.Client, error) {
 }
 
 func Address() string {
-	envAddr := os.Getenv("CONTAINERD_ADDRESS")
-	if envAddr != "" {
-		return envAddr
-	}
-
-	if os.Getpid() == 0 {
-		return defaults.DefaultAddress
-	}
-
-	// look for rootless address (fallback to default if not found)
-	//export CONTAINERD_ADDRESS=/proc/$(cat $XDG_RUNTIME_DIR/containerd-rootless/child_pid)/root/run/containerd/containerd.sock
-
-	p := fmt.Sprintf("%s/containerd-rootless/child_pid", xdg.RuntimeDir)
-	if _, err := os.Stat(p); err != nil {
-		return defaults.DefaultAddress
-	}
-
-	by, err := os.ReadFile(p)
+	address, err := getAddress(afero.NewOsFs(), xdg.RuntimeDir, defaults.DefaultAddress)
 	if err != nil {
-		return defaults.DefaultAddress
+		return ""
 	}
-
-	if len(by) == 0 {
-		return defaults.DefaultAddress
-	}
-
-	return fmt.Sprintf("/proc/%s/root/run/containerd/containerd.sock", string(by))
+	return address
 }
 
 func Namespace() string {
@@ -55,4 +38,62 @@ func Namespace() string {
 	}
 
 	return namespace
+}
+
+func getAddress(fs afero.Fs, xdgRuntimeDir, defaultSocketPath string) (string, error) {
+	var addr string
+	if v, found := os.LookupEnv("CONTAINERD_ADDRESS"); found && v != "" {
+		addr = v
+	}
+
+	if addr != "" {
+		return addr, nil
+	}
+
+	candidateAddresses := []string{
+		// default rootless address
+		rootlessSocketPath(fs, xdgRuntimeDir),
+
+		// typically accessible to only root, but last ditch effort
+		defaultSocketPath,
+	}
+
+	for _, candidate := range candidateAddresses {
+		if candidate == "" {
+			continue
+		}
+		log.WithFields("path", candidate).Trace("trying containerd socket")
+		_, err := fs.Stat(candidate)
+		if err == nil {
+			addr = fmt.Sprintf("unix://%s", candidate)
+			break
+		}
+	}
+
+	if addr == "" {
+		return "", ErrNoSocketAddress
+	}
+
+	return addr, nil
+}
+
+func rootlessSocketPath(fs afero.Fs, xdgRuntimeDir string) string {
+	// look for rootless address (fallback to default if not found)
+	//export CONTAINERD_ADDRESS=/proc/$(cat $XDG_RUNTIME_DIR/containerd-rootless/child_pid)/root/run/containerd/containerd.sock
+
+	p := fmt.Sprintf("%s/containerd-rootless/child_pid", xdgRuntimeDir)
+	if _, err := fs.Stat(p); err != nil {
+		return ""
+	}
+
+	by, err := afero.ReadFile(fs, p)
+	if err != nil {
+		return ""
+	}
+
+	if len(by) == 0 {
+		return ""
+	}
+
+	return fmt.Sprintf("/proc/%s/root/run/containerd/containerd.sock", string(by))
 }
