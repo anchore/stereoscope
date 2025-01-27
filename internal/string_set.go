e012a24fa35d6c53e53be26a66e1cda34dc20485
Rez Moss <hi@rezmoss.com>
2025-01-27 12:29:40 -0500

    Replace manual map clearing loop with built-in clear()

--- internal/string_set.go
+++ internal/string_set.go
@@ -41,10 +41,7 @@ func (s StringSet) Contains(i string) bool {
 }
 
 func (s StringSet) Clear() {
-	// TODO: replace this with the new 'clear' keyword when it's available in go 1.20 or 1.21
-	for i := range s {
-		delete(s, i)
-	}
+	clear(s)
 }
 
 func (s StringSet) List() []string {
