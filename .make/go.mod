25a7290ef08a87b98c1c3893e1c54d1060eee024
Alex Goodman <wagoodman@users.noreply.github.com>
2026-04-08 11:50:31 -0400

    port to common patterns
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

---
+++ .make/go.mod
@@ -0,0 +1,12 @@
+module github.com/anchore/stereoscope/.make
+
+go 1.26.2
+
+require github.com/anchore/go-make v0.1.0
+
+require (
+	github.com/bmatcuk/doublestar/v4 v4.10.0 // indirect
+	github.com/goccy/go-yaml v1.19.2 // indirect
+	golang.org/x/mod v0.34.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+)
---
+++ .make/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	. "github.com/anchore/go-make"
+	"github.com/anchore/go-make/tasks/golint"
+	"github.com/anchore/go-make/tasks/gotest"
+	"github.com/anchore/go-make/tasks/release"
+)
+
+func main() {
+	Makefile(
+		gotest.Tasks(),
+		golint.Tasks(),
+		release.ChangelogTask(),
+		release.TagAndCreateGHRelease(),
+	)
+}
